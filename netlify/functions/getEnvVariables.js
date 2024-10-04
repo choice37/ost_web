7942fe7e5df1006e9dffa007f1b158613f740a08
choice37 <suntaek.oh@kt.com>
2024-10-04 15:54:21 +0900

    .

--- netlify/functions/getEnvVariables.js
+++ netlify/functions/getEnvVariables.js
@@ -1,8 +1,8 @@
 // netlify/functions/getEnvVariables.js
 exports.handler = async function(event, context) {
-    const repoOwner = process.env.REPO_OWNER;  // 환경변수에서 repoOwner 가져오기
-    const repoName = process.env.REPO_NAME;    // 환경변수에서 repoName 가져오기
-    const accessToken = process.env.ACCESS_TOKEN; // 환경변수에서 accessToken 가져오기
+    const repoOwner = process.env.repoOwner;  // 환경변수에서 repoOwner 가져오기
+    const repoName = process.env.repoName;    // 환경변수에서 repoName 가져오기
+    const accessToken = process.env.accessToken; // 환경변수에서 accessToken 가져오기
   
     return {
       statusCode: 200,
