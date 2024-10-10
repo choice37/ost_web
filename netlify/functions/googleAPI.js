20b585d94c6d3cf4c85c8f1d9b03346aa5dd1b0d
choice37 <suntaek.oh@kt.com>
2024-10-11 08:31:53 +0900

    .

--- netlify/functions/googleAPI.js
+++ netlify/functions/googleAPI.js
@@ -16,8 +16,11 @@ class GoogleSheet {
     }
 
     async authenticate() {
-        const credentials = JSON.parse(fs.readFileSync('bgm-537-b9fa3d50e755.json'));
-        const { client_email, private_key } = credentials;
+        // const credentials = JSON.parse(fs.readFileSync('bgm-537-b9fa3d50e755.json'));
+        // const { client_email, private_key } = credentials;
+        const client_email = process.env.client_email;  // 환경변수에서 repoOwner 가져오기
+        const private_key = process.env.private_key;    // 환경변수에서 repoName 가져오기
+        console.log(client_email);
 
         const auth = new google.auth.JWT({
             email: client_email,
